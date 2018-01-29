4f4f8fc2be206d58f45396517b9901ba729c80d0
Hajime Nakagami <nakagami@gmail.com>
2018-01-29 19:34:25 +0900

    refactoring

--- djfirebirdsql/operations.py
+++ djfirebirdsql/operations.py
@@ -19,17 +19,6 @@ def _substr_as_sql(self, compiler, connection, function=None, template=None, arg
         arg_sql, arg_params = compiler.compile(arg)
         sql_parts.append(arg_sql)
         params.extend(arg_params)
-    data = {**self.extra, **extra_context}
-    # Use the first supplied value in this order: the parameter to this
-    # method, a value supplied in __init__()'s **extra (the value in
-    # `data`), or the value defined on the class.
-    if function is not None:
-        data['function'] = function
-    else:
-        data.setdefault('function', self.function)
-    template = template or data.get('template', self.template)
-    arg_joiner = arg_joiner or data.get('arg_joiner', self.arg_joiner)
-    data['expressions'] = data['field'] = arg_joiner.join(sql_parts)
 
     if len(sql_parts) == 2:
         template = 'SUBSTRING(%s FROM %s)' % (sql_parts[0], sql_parts[1])
