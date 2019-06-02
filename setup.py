486f24172a940fac31b451ce0e92446601bdea37
Hajime Nakagami <nakagami@gmail.com>
2019-06-02 18:39:55 +0900

    revision number up

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 ]
 
 setup(name='djfirebirdsql', 
-        version='0.0.3',
+        version='0.0.4',
         description='Django database backend for firebird',
         long_description=open('README.rst').read(),
         url='https://github.com/nakagami/djfirebirdsql/',
