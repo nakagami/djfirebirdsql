fc91814b8f7ca9c04246597c688291400da2a089
Hajime Nakagami <nakagami@gmail.com>
2019-09-19 23:01:22 +0900

    connection_persists_old_columns = True

--- djfirebirdsql/features.py
+++ djfirebirdsql/features.py
@@ -34,3 +34,4 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_atomic_references_rename = False
     supports_column_check_constraints = False
     max_query_params = 999
+    connection_persists_old_columns = True
