a4f8c1e52704e19995db11f46a95a8919e7b83f5
Hajime Nakagami <nakagami@gmail.com>
2020-04-29 11:36:12 +0900

    fix client

--- djfirebirdsql/client.py
+++ djfirebirdsql/client.py
@@ -7,7 +7,7 @@ from django.db.backends.base.client import BaseDatabaseClient
 class DatabaseClient(BaseDatabaseClient):
 
     @classmethod
-    def runshell_db(cls, conn_params):
+    def settings_to_cmd_args(cls, settings_dict, parameters):
         if os.path.exists('/usr/bin/isql-fb'):
             executable_name = 'isql-fb'
         else:
@@ -15,16 +15,24 @@ class DatabaseClient(BaseDatabaseClient):
 
         args = [executable_name]
 
-        args.append(params['database'])
+        database = settings_dict['OPTIONS'].get('database', settings_dict['NAME'])
+        user = settings_dict['OPTIONS'].get('user', settings_dict['USER'])
+        passwd = settings_dict['OPTIONS'].get('password', settings_dict['PASSWORD'])
+        passwd = settings_dict['OPTIONS'].get('ROLE')
+        host = settings_dict['OPTIONS'].get('host', settings_dict['HOST'])
+        port = settings_dict['OPTIONS'].get('port', settings_dict['PORT'])
 
-        if params['user']:
-            args += ["-u", params['user']]
-        if params['password']:
-            args += ["-p", params['password']]
-        if 'role' in params:
-            args += ["-r", params['role']]
+        args.append(database)
 
-        subprocess.check_call(args)
+        if user:
+            args += ["-u", user]
+        if password:
+            args += ["-p", password]
+        if role:
+            args += ["-r", role]
 
-    def runshell(self):
-        DatabaseClient.runshell_db(self.connection.get_connection_params())
+        args.extend(parameters)
+
+    def runshell(self, parameters):
+        aegs = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict, parameters)
+        subprocess.run(args, chrck=True)
