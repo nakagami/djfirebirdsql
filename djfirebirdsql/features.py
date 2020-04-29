13d1a25990d60df534c972da11f53fffa7f90b2e
Hajime Nakagami <nakagami@gmail.com>
2020-04-29 12:13:22 +0900

    supports_timezones = True

--- djfirebirdsql/features.py
+++ djfirebirdsql/features.py
@@ -23,7 +23,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_over_clause = True
     has_bulk_insert = False
     can_introspect_duration_field = False
-    supports_timezones = False
+    supports_timezones = True
     has_zoneinfo_database = False
     supports_select_intersection = False
     supports_select_difference = False
