69d3b0d9902738d0330fb63b5afa07f9e14600af
Hajime Nakagami <nakagami@gmail.com>
2020-07-25 18:03:26 +0900

    remove _check_active_connection()

--- djfirebirdsql/creation.py
+++ djfirebirdsql/creation.py
@@ -62,7 +62,6 @@ class DatabaseCreation(BaseDatabaseCreation):
         return test_database_name
 
     def _destroy_test_db(self, test_database_name, verbosity):
-        self._check_active_connection(verbosity)
         connection = Database.connect(**self._get_connection_params(database=test_database_name))
         connection.drop_database()
         connection.close()
