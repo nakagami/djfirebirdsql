0d33dd0b7b4ebb0151c2a5094f05fb15203d5a27
Hajime Nakagami <nakagami@gmail.com>
2020-08-01 15:23:15 +0900

    fix introspected_field_types()

--- djfirebirdsql/features.py
+++ djfirebirdsql/features.py
@@ -43,6 +43,9 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def introspected_field_types(self):
         return {
             **super().introspected_field_types,
+            'PositiveBigIntegerField': 'BigIntegerField',
+            'PositiveIntegerField': 'IntegerField',
+            'PositiveSmallIntegerField': 'SmallIntegerField',
             'DurationField': 'BigIntegerField',
             'GenericIPAddressField': 'CharField',
         }
